b35528af8a43a19e5589c00f60438e277464e64e
Khazam13 <48281401+Khazam13@users.noreply.github.com>
2019-10-21 14:00:55 +0200

    Add files via upload

---
+++ src/main/java/com/cpu_ag/Person.java
@@ -0,0 +1,62 @@
+package com.cpu_ag;
+
+import java.util.Objects;
+
+public class Person {
+
+    String name;
+    /**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	String surname;
+
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    public Person() {
+	}
+ 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(surname, person.surname);
+    }
+ 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
---
+++ src/test/java/com/cpu_ag/NativeQuarkusEndpointIT.java
@@ -0,0 +1,9 @@
+package com.cpu_ag;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeQuarkusEndpointIT extends QuarkusEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
